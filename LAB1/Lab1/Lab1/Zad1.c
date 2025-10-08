842ee4a52f3006560088591e3914f93aef843c1c
Kneza20 <lknezevic519@gmail.com>
2025-10-09 00:28:30 +0200

    2. dio zadatka

--- LAB1/Lab1/Lab1/Zad1.c
+++ LAB1/Lab1/Lab1/Zad1.c
@@ -11,27 +11,46 @@ broj bodova na kolokviju. relatvan_br_bodova = br_bodova/max_br_bodova*100*/
 #include <stdlib.h>
 
 typedef struct student {
-	char ime[20];
-	char prezime[20];
-	int brojBodova;
+	char name[20];
+	char surname[20];
+	int pointsNum;
 }Student;
 
-int brojStud(int i) {     //fja za prebrojavanje studenata
-	FILE* fp;
-	char buffer[MAX_LINE];
-	fp = fopen("doc.txt", "r");
-	while (fgets(buffer,MAX_LINE, fp) != NULL) {
-		i = i+1;
-	}
-	fp = fclose(fp);
-	return i;
-}
+int studentNum(int i);
+int studentStat(int studentNum, Student* s);
 
 int main() {
 	int i = 0;
 	Student s;
 
-	printf("U datoteci je zapisano %d studenata.\n", brojStud(i));
+	int k = studentNum(i);
+	printf("U datoteci je zapisano %d studenata.\n", k);
+
+	studentStat(k, &s);
 
 	return 0;
+}
+
+int studentNum(int i) {     //fja za prebrojavanje studenata
+	FILE* fp;
+	char buffer[MAX_LINE];
+	fp = fopen("doc.txt", "r");
+	while (fgets(buffer, MAX_LINE, fp) != NULL) {
+		i = i + 1;
+	}
+	fp = fclose(fp);
+	return i;
+}
+
+int studentStat(int studentNum, Student* s) {
+	FILE* fp;
+	s = (Student*)malloc(studentNum * sizeof(Student));  //alokacija memorije velicine brojStud
+	fp = fopen("doc.txt", "r");
+	for (int i = 0; i < studentNum; i++) {
+		fscanf(fp, "%s %s %d", s[i].name, s[i].surname, s[i].pointsNum);
+		int relPointsNum = (s[i].pointsNum / 100) * 100;  //relativni broj bodova
+		printf("Student %s %s je ostvario apsolutni broj bodova %d  i relativni broj bodova %d\n", s[i].name, s[i].surname, s[i].pointsNum, relPointsNum);
+	}
+	fp = fclose(fp);
+	free(s);
 }
