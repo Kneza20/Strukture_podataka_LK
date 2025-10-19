1ecf6443b549839713c904b5fa67e8a9fc2b9305
Kneza20 <lknezevic519@gmail.com>
2025-10-19 11:47:49 +0200

    added functions

--- LAB1/Lab1/Lab1/Zad2.c
+++ LAB1/Lab1/Lab1/Zad2.c
@@ -5,19 +5,53 @@ C. dinamicki dodaje novi element na kraj liste,
 D. pronalazi element u listi (po prezimenu), 
 E. brise odredjeni element iz liste, U zadatku se ne smiju koristiti globalne varijable.*/
 #define _CRT_SECURE_NO_WARNINGS
+#define MAX_LINE (1024)
+#define ERROR_FILE_NOT_OPENED (-1)
+#define ERROR_MEMORY_ALLOCATION (-2)
 #include <stdio.h>
 #include <stdlib.h>
 
-#define personBuffer (20)
+typedef struct Person* newPerson;
 
 typedef struct Person {
-	char name[personBuffer];
-	char surname[personBuffer];
+	char name[MAX_LINE];
+	char surname[MAX_LINE];
 	int yearOfBirth;
+	newPerson Next;
 }person;
 
+int memoryAlloc(newPerson P) {
+	P = (newPerson)malloc(sizeof(person));
+	if (P == NULL) {
+		printf("Error allocating memory!!\n");
+		return ERROR_MEMORY_ALLOCATION;
+	}
+	else {
+		return P;
+	}
+}
+
+int addFirst(char name[MAX_LINE], char surname[MAX_LINE], int yearOfBirth, newPerson P);
+
+int addLast(char name[MAX_LINE], char surname[MAX_LINE], int yearOfBirth, newPerson P);
+
+int printOut(newPerson P);
+
+int searchBySurname(char surname[MAX_LINE], newPerson P);
+
+int erase(char name[MAX_LINE], char surname[MAX_LINE], int yearOfBirth, newPerson P);
+
 int main() {
-	person* p=NULL;
+	newPerson head = NULL;
+	head = memoryAlloc(head);
+
+	head->Next;
+	addFirst("Luka", "Knezevic", 2005, head);
+	addLast("Ana", "Beslic", 2007, head);
 
 	return 0;
+}
+
+int addFirst(char name[MAX_LINE], char surname[MAX_LINE], int yearOfBirth, newPerson P) {
+
 }
