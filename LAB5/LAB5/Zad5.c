e99ae20afe3a9cca4b8aa8b1e0b801d4c2868943
Kneza20 <lknezevic519@gmail.com>
2025-11-11 14:49:16 +0100

    result of postfix is wrong

--- LAB5/LAB5/Zad5.c
+++ LAB5/LAB5/Zad5.c
@@ -9,6 +9,7 @@ Stog je potrebno realizirati preko vezane liste.*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 
 typedef struct postfix* Position;
@@ -20,13 +21,18 @@ typedef struct postfix {
 
 Position memoryAlloc();
 
-int push(Position P, int num);
+Position push(Position P, int num);
 
-int pop(Position P, int num);
+int pop(Position P);
 
-int calculate(int first, int second, char operand);
+int calculate(Position P);
 
 int main() {
+	Position P = NULL;
+	P = memoryAlloc();
+	int result = 0;
+	result = calculate(P);
+	printf("Result of postfix equation is: %d", result);
 
 	return EXIT_SUCCESS;
 }
@@ -38,9 +44,10 @@ Position memoryAlloc() {
 		printf("Error allocating memory");
 		return ERROR_ALLOCATION;
 	}
+	return Q;
 }
 
-int push(Position P, int num) {
+Position push(Position P, int num) {
 	Position newElement = NULL;
 	newElement = memoryAlloc();
 
@@ -50,36 +57,62 @@ int push(Position P, int num) {
 	return newElement;
 }
 
-int pop(Position P, int num) {
+int pop(Position P) {
 	if (P == NULL) {
 		printf("Stack is empty!");
 		return ERROR_EMPTY_STACK;
 	}
 
-	Position temp = P; //trenutni vrh stoga spremamo u varijablu temp
-	P = P->Next;       //pomicanje na sljedeći element stoga
-	free(temp);		   //brisanje prethodnog vrha stoga
-	return P;
+	int num = (P)->number;
+	Position temp = P;
+	P = P->Next;
+	free(temp);
+	return num;
 }
 
-int calculate(int first, int second, char operand) {
-	switch (operand) {
-	case '+': return first + second;
-	case '-': return first - second;
-	case '*': return first * second;
-	case '/':
-		if (second = 0) {
-			printf("Can not divide by zero!\n");
-			return ERROR_DIVIDING;
+int calculate(Position P) {
+	int i = 0;
+	FILE* fp = NULL;
+	fp = fopen("dat.txt", "r");
+
+	char buffer[MAX_LINE];
+
+	while (fscanf(fp,"%s",buffer)==1) {
+		if (isdigit(buffer[0]) || (buffer[0] == '-' && isdigit(buffer[0]))) {    //uvjet za provjeravanje da li je element broj i to da li je poz ili neg
+			int number;
+			number = buffer[i];
+			P = push(P, number);
 		}
 		else {
-			return first / second;
+			int result = 0;
+			int first = pop(P);
+			int second = pop(P);
+
+			switch (buffer[0]) {
+			case '+': result = second + first; break;
+			case '-': result = second - first; break;
+			case '*': result = second * first; break;
+			case '/':
+				if (first = 0) {
+					printf("Can not divide by zero!\n");
+					return ERROR_DIVIDING;
+				}
+				else {
+					result = second / first;
+					break;
+				}
+
+			default:
+				printf("Unknown operand!\n");
+				return ERROR_OPERAND;
+			}
+			P = push(P, result);
 		}
-
-	default:
-		printf("Unknown operand!\n");
-		return ERROR_OPERAND;
+		i++;
 	}
+	fclose(fp);
 
+	int finalResult = pop(P);
 
+	return finalResult;
 }
